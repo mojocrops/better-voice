eca1a92bfb12615b35f6a0ba2609ea54f2742e00
mojocrops <mojocrops@gmail.com>
2025-04-10 14:02:12 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    // Fix for WebSocket polyfills
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        bufferutil: false,
+        'utf-8-validate': false,
+      };
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
